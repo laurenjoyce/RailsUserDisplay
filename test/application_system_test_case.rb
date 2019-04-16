3f0c610bec1b20a7c72afdb8f55c76f550ae92a1
Lauren Joyce <joycelc4186@gmail.com>
2019-04-16 18:03:04 -0500

    Capybara acceptance testing for the UI

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,4 +2,43 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+
+  def setup
+    @user1 = users(:one)
+    @user2 = users(:two)
+  end
+
+  test 'user index' do
+    visit users_path
+
+    assert page.has_content?(@user1.name)
+    assert page.has_content?(@user2.name)
+  end
+
+  test 'add a new user' do
+    visit users_path
+    click_link('New User')
+    fill_in('Name', :with => 'Foo')
+    click_button('Create User')
+
+    assert page.has_content?("Foo")
+  end
+
+  test 'update a user' do
+    visit users_path
+    find_link('Edit', href: "/users/#{@user1.id}/edit").click
+    fill_in('Name', :with => "Bar")
+    click_button('Update User')
+
+    assert page.has_content?(@user1.id)
+    assert page.has_content?("Bar")
+  end
+
+  test 'user show' do
+    visit users_path
+    find_link('Show', href: "/users/#{@user1.id}").click
+
+    assert page.has_content?(@user1.id)
+    assert page.has_content?(@user1.name)
+  end
 end
